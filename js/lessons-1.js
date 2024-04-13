9411fbb01126ff507e5d7b37162f2990727ba6ac
Hanna <tarasenko.ganna.1998@gmail.com>
2024-04-13 13:33:33 +0100

    update lesson1

--- js/lessons-1.js
+++ js/lessons-1.js
@@ -9,8 +9,17 @@
 
 //ЗАДАЧА 2
 //ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ, ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
-const names = prompt("Введіть своє ім'я");
-const post = prompt("Введіть вашу пошту");
-const telef = prompt("Введіть ваш телефонний номер");
-const message = `КОРИСТУВАЧ ${names} ВИКОРИСТОВУЄ  ${post} ПОЧТУ І ${telef} ТЕЛЕФОН`;
-console.log(message);
+// const names = prompt("Введіть своє ім'я");
+// const post = prompt("Введіть вашу пошту");
+// const telef = prompt("Введіть ваш телефонний номер");
+// const message = `КОРИСТУВАЧ ${names} ВИКОРИСТОВУЄ  ${post} ПОЧТУ І ${telef} ТЕЛЕФОН`;
+// console.log(message);
+
+// ЗАДАЧА 3
+// ПОТРІБНО ЗАПИТАТИ У МАГАЗИНІ ЦІНУ ЗА КОЖЕН ОКРЕМИЙ ТОВАР,ТА ВИВЕСТИ ПОВІДОМЛЕННЯМ   ЗАГАЛЬНУ ЦІНУ ТОВАРУ
+
+const price1 = Number(prompt("Введіть ціну товару1"));
+const price2 = +prompt("Введіть ціну товару2");
+const price3 = +prompt("Введіть ціну товару3");
+const totalPrice = price1 + price2 + price3;
+console.log(totalPrice);
