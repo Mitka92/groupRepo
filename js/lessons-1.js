818e7b050fd2455c4748ba76574ab9f0413c60c5
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-04-13 15:12:22 +0300

    task-1

---
+++ js/lessons-1.js
@@ -0,0 +1,7 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+const name = "Галина";
+const age = 35;
+const hobbi = "Javascript";
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobbi}`;
+console.log(message);
