990fed4b498a32e8332d5b818b9a7b8d05bd503d
Serhii <158744795+BurglachOK@users.noreply.github.com>
2024-04-13 14:24:03 +0200

    serhii

--- js/lessons-1.js
+++ js/lessons-1.js
@@ -1,7 +1,16 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = "Галина";
-const age = 35;
-const hobbi = "Javascript";
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobbi}`;
+//const name = "Галина";
+//const age = 35;
+//const hobbi = "Javascript";
+//const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobbi}`;
+//console.log(message);
+
+
+//ЗАДАЧА 2
+//ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ, ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+const names = prompt("Введіть своє ім'я");
+const post = prompt("Введіть вашу пошту");
+const telef = prompt("Введіть ваш телефонний номер");
+const message = `КОРИСТУВАЧ ${names} ВИКОРИСТОВУЄ  ${post} ПОЧТУ І ${telef} ТЕЛЕФОН`;
 console.log(message);
