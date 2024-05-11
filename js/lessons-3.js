2abdc0894b560e8433611fa95798417c189fafb2
Sergey Stupak <itstupak@gmail.com>
2024-05-11 14:14:57 +0200

    task-1

---
+++ js/lessons-3.js
@@ -0,0 +1,14 @@
+// ЗАДАЧА 1
+// Напишіть дві функції:
+// letMeSeeYourName(callback) - запитує імя користувача , через prompt і викликає колбек функцію (callback)
+// greet(name) - колбек, який приймає імя і логінує в консоль рядок  "Привіт" + імя
+
+// function letMeSeeYourName(callback) {
+//     const userName = prompt("Введіть ім'я");
+//     callback(userName);
+// }
+// function greet(name) {
+//     console.log(`Привіт ${name}`);
+// };
+
+// letMeSeeYourName(greet);
