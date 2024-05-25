c79e794ad16ccebe8402cff92116fcaa1c1a8f31
Sergey Stupak <itstupak@gmail.com>
2024-05-25 14:48:13 +0200

    task-1 lesson-4

--- js/lessons-4.js
+++ js/lessons-4.js
@@ -2,5 +2,23 @@
 // Створіть карточки товару з масиву використовуючи createElement
 
 import carList from './date/car.js'
-console.log(carList);
 
+function carTamplate({ name, img, price, description }) {
+    const item = document.createElement('li');
+    const title = document.createElement('h2');
+    title.textContent = name;
+    const picture = document.createElement('img');
+    picture.src = img;
+    picture.alt = name;
+    picture.classList.add('item-img');
+    const dscr = document.createElement('p');
+    dscr.textContent = description;
+    const priceEl = document.createElement('p');
+    priceEl.textContent = price;
+    item.append(picture, title, priceEl, dscr);
+    return item;
+}
+
+const elements = carList.map(carTamplate);
+
+const markup = document.querySelector('.js-goods'); markup.append(...elements);
